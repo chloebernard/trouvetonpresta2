db1787c25a0b0305f59d9b1d43c6d51ab2520035
tomdeserter <tom.wiltberger@essec.edu>
2017-03-28 15:43:40 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,8 +4,8 @@
 		$email = $_POST['email'];
 		$message = $_POST['message'];
 		$human = intval($_POST['human']);
-		$from = 'Demo Contact Form'; 
-		$to = 'example@domain.com'; 
+		$from = 'Trouve ton presta'; 
+		$to = 'tom.wiltberger@essec.edu'; 
 		$subject = 'Message from Contact Demo ';
 		
 		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
@@ -24,10 +24,7 @@
 		if (!$_POST['message']) {
 			$errMessage = 'Please enter your message';
 		}
-		//Check if simple anti-bot test is correct
-		if ($human !== 5) {
-			$errHuman = 'Your anti-spam is incorrect';
-		}
+		
 
 // If there are no errors, send the email
 if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
